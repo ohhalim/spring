de2192f1b3c032226133a0e45bca0428b3f92f23
ohhalim <ohhalim777@gmail.com>
2026-04-07 00:09:09 +0900

    refactor(payment): 클라이언트에서 객체 조립 책임 분리해 ObjectFactory 통해 PaymentService 주입
    - Client에서 PaymentService 직접 생성 로직 제거
    - ObjectFactory 인스턴스를 통해 paymentService()를 호출해 결제 서비스 획득
    - 객체 생성/의존성 바인딩 책임을 ObjectFactory로 이동해 진입점 책임 축소
    - ObjectFactroy 오타를 ObjectFactory로 정정

--- hellospring/src/main/java/com/tobyspring/hellospring/Client.java
+++ hellospring/src/main/java/com/tobyspring/hellospring/Client.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        ObjectFactory objectFastroy = new ObjectFactory();
+        PaymentService paymentService = objectFastroy.paymentService();
+
         Payment payment = paymentService.prepare(100L,"USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
---
+++ hellospring/src/main/java/com/tobyspring/hellospring/ObjectFactory.java
@@ -0,0 +1,10 @@
+package com.tobyspring.hellospring;
+
+public class ObjectFactory {
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+    public ExRateProvider exRateProvider() {
+        return new WebApiExRateProvider();
+    }
+}
