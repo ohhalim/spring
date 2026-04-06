20d8ca024516ccb061a546bd506621c3b9394ca5
ohhalim <ohhalim777@gmail.com>
2026-04-06 17:55:50 +0900

    feat: 기본 결제 코드 연습

---
+++ hellospring/src/main/java/com/tobyspring/hellospring/Payment.java
@@ -0,0 +1,58 @@
+package com.tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Payment {
+    private Long orderId;
+    private String currency;
+    private BigDecimal foreignCurrencyAmount;
+    private BigDecimal exRate;
+    private BigDecimal convertedAmount;
+    private LocalDateTime vaildUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exRate, BigDecimal convertedAmount, LocalDateTime vaildUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exRate = exRate;
+        this.convertedAmount = convertedAmount;
+        this.vaildUntil = vaildUntil;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public BigDecimal getForeignCurrencyAmount() {
+        return foreignCurrencyAmount;
+    }
+
+    public BigDecimal getExRate() {
+        return exRate;
+    }
+
+    public BigDecimal getConvertedAmount() {
+        return convertedAmount;
+    }
+
+    public LocalDateTime getVaildUntil() {
+        return vaildUntil;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "orderId=" + orderId +
+                ", currency='" + currency + '\'' +
+                ", foreignCurrencyAmount=" + foreignCurrencyAmount +
+                ", exRate=" + exRate +
+                ", convertedAmount=" + convertedAmount +
+                ", vaildUntil=" + vaildUntil +
+                '}';
+    }
+}
---
+++ hellospring/src/main/java/com/tobyspring/hellospring/PaymentService.java
@@ -0,0 +1,21 @@
+package com.tobyspring.hellospring;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class PaymentService {
+    public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) {
+        // 환율 가져오기
+        // 금액 계산
+        // 유효시 계
+        return new Payment(orderId, currency, foreignCurrencyAmount, BigDecimal.ZERO, BigDecimal.ZERO,
+                LocalDateTime.now() );
+    }
+
+    public static void main(String[] args) {
+        PaymentService paymentService = new PaymentService();
+        Payment payment = paymentService.prepare(100L,"USD",BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
+                                                                          
